abc3a5b4f8ad8fc9397372dec710293b046b1447
klaud <klaudia.szelag@vp.pl>
2023-01-13 23:46:43 +0100

    lab1

---
+++ src/main/java/pl/edu/wszib/lab1/builder/record/Address.java
@@ -0,0 +1,28 @@
+package pl.edu.wszib.lab1.builder.record;
+
+import com.google.auto.value.AutoBuilder;
+
+public record Address(
+        String city,
+        String street,
+        String number,
+        String postalCode
+) {
+
+    static Builder builder() {
+        return new AutoBuilder_Address_Builder();
+    }
+
+    @AutoBuilder
+    interface Builder {
+        Builder city(String city);
+
+        Builder street(String street);
+
+        Builder number(String number);
+
+        Builder postalCode(String postalCode);
+
+        Address build();
+    }
+}
